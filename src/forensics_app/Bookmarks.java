ac665fc6d9d032e7b7550d2410bf68afe54039ad
AyaMoaaz <aya.moaaz@gmail.com>
2017-02-27 14:36:45 +0200

    Merge origin/test into HEAD
    Conflicts:
    	Forensic_project/src/forensic_project/Forensic_project.java

--- src/forensics_app/Bookmarks.java
+++ src/forensics_app/Bookmarks.java
@@ -6,23 +6,92 @@
 package forensics_app;
 
 import java.util.ArrayList;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 /**
  *
  * @author Ayaa
  */
 public class Bookmarks {
+    public static String jsonFile = "C:\\Users\\Ayaa\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\bookmarks";
+    static int count = 0;
     /*Description  : bnrg3 l data mn l sqllite file fe resultset 3adi 
       * we b3d keda bn3ml list mn l object DownloadContent we nbd2 nfadi l resultset f objects
       * return : list of objects from DownloadContent
       */
     // mal7oza l time hena l mfrod yrg3 3ala hy2t time stamp 3ala tol 
-      ArrayList<BookmarksContent>  RetrunData(String time) 
+    /*  ArrayList<BookmarksContent>*/void  RetrunData() 
     {
-       
-     ArrayList<BookmarksContent> listOf_bookmarks_content=new ArrayList();
-     return listOf_bookmarks_content;
+    
+        // a file reader class to access the file using string file path 
+        FileReader reader = null;
+        try {
+            reader = new FileReader(jsonFile);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } // access the file
+        JSONObject jsonObject = null;
+
+        try {
+            jsonObject = (JSONObject) new JSONParser().parse(reader);
+        } catch (IOException | ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String checksum = (String) jsonObject.get("checksum");
+
+        JSONObject root = (JSONObject) jsonObject.get("roots");
+
+        Set<String> set = root.keySet();
+        JSONArray childrens = null;
+        JSONObject obj = null;
+        for (String string : set) {
+            try {
+                obj = (JSONObject) root.get(string);
+            } catch (ClassCastException e) {
+
+            }
+            if (obj.containsKey("children")) {
+                try {
+                    childrens = (JSONArray) obj.get("children");
+                    printContent(childrens);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        // display , how many urls we have found  
+        System.out.println("count is " + count);
+    }
+    public static void printContent(JSONArray childrens) {
+        JSONObject temp = null;
+        for (int i = 0; i < childrens.size(); i++) {
+            // get object using index from childrens array
+            temp = (JSONObject) childrens.get(i);
+            if (temp.containsKey("children")) {
+                printContent((JSONArray) temp.get("children"));
+            }
+            // get url
+            String url = (String) temp.get("url");
+            String name = (String) temp.get("name");
+            String type = (String) temp.get("type");
+            String date_added = (String) temp.get("date_added");
+            
+            if (url != null) {
+                System.out.println("url: "+url+"\n"+"name: "+ name+"\n"+"type: "+type+"\n"+"date added: "+date_added);
+                count++;
+            }
+//     ArrayList<BookmarksContent> listOf_bookmarks_content=new ArrayList();
+//     return listOf_bookmarks_content;
         
+    }
     }
     /*
       *nafs l fkra feli 3mlnah fel hisotry hn3ml call le function l return data 
